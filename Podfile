8d6fa6cb86bc35e5b84c56ba73d02dc8aaf2c624
askari01 <farrukh.askari01@gmail.com>
2017-09-11 20:14:42 +0500

    first reactive program

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RXSwift Intro' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RXSwift Intro
+  pod 'RxSwift',    '~> 3.0'
+  pod 'RxCocoa',    '~> 3.0'
+end
--- RXSwift Intro/ViewController.swift
+++ RXSwift Intro/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstTF: UITextField!
+    @IBOutlet weak var secondTF: UITextField!
+    @IBOutlet weak var thirdTF: UITextField!
+    @IBOutlet weak var resultTF: UITextField!
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Observable.combineLatest(firstTF.rx.text.orEmpty, secondTF.rx.text.orEmpty, thirdTF.rx.text.orEmpty) { textValue1, textValue2, textValue3 -> Int in
+            return ((Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0))
+        }
+            .map{ $0.description }
+            .bind(to: resultTF.rx.text)
+            .disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
